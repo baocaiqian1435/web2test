721b09b1340cee9900d20942b72e04272bf38fe3
baocaiqian1435 <15226535887@163.com>
2017-11-27 21:20:58 +0800

    去掉注释以后

--- 实验四/js.js
+++ 实验四/js.js
@@ -3,17 +3,6 @@ var img1=document.getElementById('picScroll1');
 var img2=document.getElementById('picScroll2');
 
 function changeToLeft() {
-    /*右移
-	//img2.innerHTML=img1.innerHTML;
-	if(img.scrollLeft<=0)
-	{
-    img.scrollLeft+=img2.offsetWidth;
-    }
-    else{
-    img.scrollLeft--;
-    }
-    */
-    //左移
     if(img.scrollLeft-img1.offsetWidth>=0)
     {
     img.scrollLeft=0;
